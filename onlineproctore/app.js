8ddcacd6c35ecaeeeca9e5ef16429d5411c84c8d
divy3011 <divyias3011@gmail.com>
2021-12-16 22:24:54 +0530

    home page added

--- onlineproctore/app.js
+++ onlineproctore/app.js
@@ -4,8 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var index = require('./routes/root/index');
+var users = require('./routes/users');
 
 var app = express();
 
@@ -24,10 +24,10 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/public')));
 
-app.use('/', indexRouter);
-// app.use('/users', usersRouter);
+app.use('/', index);
+app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
